0a6eaa15437ef5cd0629639c944069547e39e69e
Mark David Teo <mark@Marks-MacBook-Pro.local>
2018-02-14 23:26:25 +0800

    added styled components with jest

--- src/HomeScreen.test.js
+++ src/HomeScreen.test.js
@@ -3,6 +3,8 @@ import { mount, render, shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import toJson from 'enzyme-to-json';
 import HomeScreen from './HomeScreen';
+import 'jest-styled-components'
+
 
 it('renders correctly', () => {
   const tree = renderer.create(<HomeScreen/>).toJSON()
